6bcc9594c2721a71b9c9a652c018c5dd5234ec70
luis-jaime-07 <136880890+luis-jaime-07@users.noreply.github.com>
2023-11-24 00:22:27 +0800

    Update StyleCourses.js

--- StyleCourses.js
+++ StyleCourses.js
@@ -1,19 +1,39 @@
-function toggleDropdown(clickedElement) {
-    var dropdown = clickedElement.closest('.dropdown');
-    var dropdownContent = dropdown.querySelector('.dropdown-content');
-    
-    dropdown.classList.toggle('active');
-    dropdownContent.classList.toggle('hover');
-  
-    // Get the second dropdown and its content
-    var dropdown2 = document.getElementById('dropdown2');
-  
-    if (dropdown1.classList.contains('active')) {
-      // If the first dropdown is active, add a class to the second dropdown to move it down
-      dropdown2.classList.add('move-down');
-    } else {
-      // If the first dropdown is retracted, remove the class to move the second dropdown back up
-      dropdown2.classList.remove('move-down');
+    function toggleDropdown(clickedElement) {
+        var dropdown = clickedElement.closest('.dropdown');
+
+        if (isOwned(clickedElement)) {
+            var dropdownContent = dropdown.querySelector('.dropdown-content');
+
+            dropdown.classList.toggle('active');
+            dropdownContent.classList.toggle('hover');
+
+            var dropdown2 = document.getElementById('dropdown2');
+
+            if (dropdown1.classList.contains('active')) {
+                dropdown2.classList.add('move-down');
+            } else {
+                dropdown2.classList.remove('move-down');
+            }
+        }
     }
-  }
-  
+
+    function buyNow(button) {
+        
+        button.innerHTML = "Owned";
+    
+        var dropdown = button.closest('.dropdown');
+        dropdown.classList.add('owned');
+    
+        var lockIcon = dropdown.querySelector('.fa-lock');
+    
+        lockIcon.classList.remove('fa-lock');
+        lockIcon.classList.add('fa-unlock');
+        var priceTag = dropdown.querySelector('.price-tag');
+        if (priceTag) {
+            priceTag.style.display = 'none';
+        }
+    }
+    
+    function isOwned(clickedElement) {
+        return clickedElement.closest('.dropdown').classList.contains('owned');
+    }
