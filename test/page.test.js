be9723c04d5587662f6745950b135dc9ff15bd0a
GcpAps <134756031+GcpAps@users.noreply.github.com>
2024-02-26 11:59:24 +0800

    Title change
    Changed title of home page to Home. For testing the landing page based on html title

--- test/page.test.js
+++ test/page.test.js
@@ -1,15 +1,16 @@
-const supertest = require("supertest");
-const assert = require('assert');
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app'); // assuming your Express app is defined in app.js
 
-describe("GET /", function() {
-  it("it should has status code 200", function(done) {
-    supertest(app)
-      .get("/")
-      .expect(200)
-      .end(function(err, res){
-        if (err) done(err);
-        done();
-      });
-  });
+
+describe('Landing page based on HTML title', () => {
+    it('should render the landing page when the title is "Home"', async () => {
+        const chai = await import('chai');
+        const expect = chai.expect;
+
+        const res = await request(app).get('/');
+        expect(res.status).to.equal(200); // 200 is the status code for a successful request
+        expect(res.text).to.include('<title>Home</title>'); // Check if the HTML title is "Home"
+    });
+
+    // Add more tests for other landing page titles as needed
 });
