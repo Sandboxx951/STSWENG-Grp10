02f756af12953b41ace26547aa1e48aa6b4b9c2c
Zerithe <jonathandevera08@gmail.com>
2024-03-08 00:00:03 +0800

    added temp handlebars (no implementation yet)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+var hbs = require('express-hbs');
 const bodyParser = require('body-parser');
 const path = require('path'); // Import the path module
 
 const app = express();
+const port = 3000;
+
+//uses handlebars for views
+app.engine('hbs', hbs.express4({partialsDir: __dirname +'/public/views/partials'}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname, '/public/views');
 
 // Middleware configurations
 app.use(bodyParser.json());
@@ -14,3 +21,5 @@ app.get('/', (req, res) => {
 });
 
 module.exports = app;
+
+app.listen(port, () => console.log('App listening to port ${port}'));
