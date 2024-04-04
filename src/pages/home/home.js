18b111947738abc9ff77bd87697c24ac8ccaaaf3
Miguel Gonzales <113465269+AstilbeTheFlower@users.noreply.github.com>
2024-04-05 03:37:13 +0800

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,5 +3,5 @@ import '../../assets/images/pic1.JPG';
 import '../../assets/test_files_idk/test_pic1.png';
 import '../../assets/test_files_idk/test_picture2.png';
 import '../../assets/test_files_idk/test_picture3.png';
-imprt '../../assets/test_files_idk/test_picture4.png';
+import '../../assets/test_files_idk/test_picture4.png';
 import '../../assets/test_files_idk/test_picture5.png';
