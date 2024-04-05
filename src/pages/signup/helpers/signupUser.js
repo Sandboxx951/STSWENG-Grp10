cbf7c5634fdb375d8c1e60b6cec204ff9d2c5182
GcpAps <134756031+GcpAps@users.noreply.github.com>
2024-04-05 13:55:02 +0800

    Update signupUser.js

--- src/pages/signup/helpers/signupUser.js
+++ src/pages/signup/helpers/signupUser.js
@@ -4,27 +4,32 @@ async function submitData(username, useremail, userpassword){
     const email = useremail;
     const password = userpassword;
 
-    try {
-            const response = await fetch('http://localhost:3000/signup', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ name, email, password }),
-            });
-        
-            if (response.status === 400) {
-              const data = await response.json();
-              alert(data.error); // Display the error message from the server
-            } else if (response.ok) {
-              const data = await response.json();
-              alert(data.message);
-              document.getElementById('signup-form').reset();
-              window.location.href = 'login.html';
-            }
-          } catch (error) {
-            console.error('Error during signup:', error);
-          }
+    const urls = ['http://localhost:3000/signup', 'https://stsweng-grp10-1.onrender.com/signup'];
+    
+    for (const url of urls) {
+        try {
+                const response = await fetch(url, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({ name, email, password }),
+                });
+            
+                if (response.status === 400) {
+                  const data = await response.json();
+                  alert(data.error); // Display the error message from the server
+                } else if (response.ok) {
+                  const data = await response.json();
+                  alert(data.message);
+                  document.getElementById('signup-form').reset();
+                  window.location.href = 'login.html';
+                  break;
+                }
+              } catch (error) {
+                console.error('Error during signup:', error);
+              }
+    }
 }
 
 module.exports = {submitData}
